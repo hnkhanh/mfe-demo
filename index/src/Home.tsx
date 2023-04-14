b464d008856ea37cd7a40069bab73e8b91196fed
Le Huy Binh <binhlh@vietmap.vn>
2023-04-14 13:09:47 +0700

    add FE

--- index/src/Home.tsx
+++ index/src/Home.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Remote from "remoteApp/App";
-import useCountStore from "remoteApp/store";
+// import useCountStore from "remoteApp/store";
 
 function Home() {
   // const [count, setCount] = useState(0);
-  const { count, setCount } = useCountStore(state => state);
+  // const { count, setCount } = useCountStore(state => state);
 
   return (
     <div className="App">
@@ -12,7 +12,7 @@ function Home() {
 
       <h1>This is the home page</h1>
       <div className="card">
-        <button onClick={() => setCount()}>count is {count}</button>
+        {/* <button onClick={() => setCount()}>count is {count}</button> */}
       </div>
       <Remote></Remote>
     </div>
--- remote/src/App.tsx
+++ remote/src/App.tsx
@@ -1,27 +1,57 @@
 import { useState } from "react";
 import reactLogo from "./img/react.svg";
 // import viteLogo from "vite.svg";
-import "./App.css";
 import Button from "./Button";
-
+import Data from './data.json';
 function App() {
-  const [bit] = useState(0);
+//   const [bit] = useState(0);
 
   return (
-    <div className="App border-red">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> Remote About from 5003</h1>
-      {bit}
-      <Button></Button>
-    </div>
-  );
+		<div className='container'>
+			<div className='row row-cols-1 row-cols-md-3 mb-3 text-center'>
+				{Data.populars.map(item => (
+					<div className='col col-sm-6 col-md-4'>
+						<div className='card mb-4 rounded-3 shadow-sm'>
+							<div className='card-header py-3'>
+								<img
+									src={item.image}
+									className='card-img-top'
+									alt='categories'
+									style={{ width: ' 100%', height: ' 250px' }}
+								/>
+							</div>
+							<div className='card-body'>
+								<h5 className='card-title'>{item.name}</h5>
+								<p className='card-text'>{item.description}</p>
+							</div>
+							<div className='card-footer'>
+								<a
+									asp-action='GetDishesbyCategory'
+									asp-route-category='@category.Name'
+									className='btn btn-dark'>
+									Take a look
+								</a>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+		// <div className='App border-red'>
+		// 	<div>
+		// 		<a href='https://vitejs.dev' target='_blank'>
+		// 			{/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+		// 			asdsadsa
+		// 		</a>
+		// 		<a href='https://reactjs.org' target='_blank'>
+		// 			<img src={reactLogo} className='logo react' alt='React logo' />
+		// 		</a>
+		// 	</div>
+		// 	<h1 className='text-success'> Remote About from 5003</h1>
+		// 	{bit}
+		// 	<Button></Button>
+		// </div>
+	);
 }
 
 export default App;
